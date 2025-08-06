7da8d93e4a11095cf9d0a0b777f654459b982644
Pedro Godoi <pedrogodoiriva@gmail.com>
2025-08-06 11:51:52 -0300

    some optimizations

--- src/Canva.tsx
+++ src/Canva.tsx
@@ -1,10 +1,16 @@
-import { Excalidraw } from '@excalidraw/excalidraw';
+import React from 'react';
+
+const Excalidraw = React.lazy(() =>
+  import('@excalidraw/excalidraw').then(module => ({ default: module.Excalidraw }))
+);
 
 function Canva() {
 
   return (
-    <div className="h-[100vh]">
-      <Excalidraw theme="dark"/>
+    <div className="h-[100vh] flex justify-center items-center bg-neutral-900 text-grey-100">
+      <React.Suspense fallback={<div>Carregando...</div>}>
+        <Excalidraw theme="dark"/>
+      </React.Suspense>
     </div>
   );
 }
